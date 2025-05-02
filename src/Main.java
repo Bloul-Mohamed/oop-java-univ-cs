761d4a508f597a6f98bb5f21eed6eb86fad1144e
Mohamed-Ridha <medbloul2002@gmail.com>
2025-05-02 16:43:43 +0100

    solve another problem in series lab

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,24 @@
-import labtwo.*;
+// File: Main.java
+
 import labtwo.*;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Library library = new Library();
+    Scolarite scolarite = new Scolarite();
+    StudentUnique student1 = new StudentUnique("Ali", "Ahmed", 1);
+    student1.setGrade(85.0);
+    student1.setGrade(90.0);
+    student1.setGrade(60.0);
+    scolarite.setStudent(student1);
 
-    boolean running = true;
-    while (running) {
-      // Display menu
-      System.out.println("\n===== LIBRARY MANAGEMENT SYSTEM =====");
-      System.out.println("1. Add a book");
-      System.out.println("2. Remove damaged books");
-      System.out.println("3. List all books");
-      System.out.println("4. Exit");
-      System.out.print("Enter your choice (1-4): ");
+    StudentUnique student2 = new StudentUnique("Mohamed", "Ridha", 2);
+    student2.setGrade(75.0);
+    student2.setGrade(80.0);
+    student2.setGrade(45.0);
+    scolarite.setStudent(student2);
 
-      // Get user choice
-      int choice = 0;
-      try {
-        choice = scanner.nextInt();
-        scanner.nextLine(); // Consume newline
-      } catch (Exception e) {
-        scanner.nextLine(); // Clear buffer on error
-        System.out.println("Please enter a valid number.");
-        continue;
-      }
-
-      // Process the choice
-      switch (choice) {
-        case 1:
-          System.out.print("Enter book title: ");
-          String title = scanner.nextLine();
-          System.out.print("Enter book author: ");
-          String author = scanner.nextLine();
-          System.out.print("Enter book condition (0-5, where 0 is damaged): ");
-          int condition = scanner.nextInt();
-          scanner.nextLine(); // Consume newline
-          Book newBook = new Book(title, author, condition);
-          library.addBook(newBook);
-          System.out.println("Book added: " + newBook.getTitle());
-          break;
-        case 2:
-          library.removeDamagedBook();
-          break;
-        case 3:
-          library.inventory();
-          break;
-        case 4:
-          running = false;
-          System.out.println("Exiting program. Goodbye!");
-          break;
-        default:
-          System.out.println("Invalid choice. Please select a number between 1 and 4.");
-      }
-    }
-
-    scanner.close();
+    System.out.println("Number of students: " + scolarite.nbStudents());
+    System.out.println("Students with average above 80:");
+    scolarite.displayAllStudents(80.0);
   }
 }
--- src/labtwo/StudentUnique.java
+++ src/labtwo/StudentUnique.java
@@ -41,4 +41,36 @@ public class StudentUnique {
     return sum / this.grades.size();
   }
 
+  public void setGrade(double grade) {
+    this.grades.add(grade);
+  }
+
+  public void setid(int id) {
+    this.id = id;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public ArrayList<Double> getGrades() {
+    return this.grades;
+  }
+
 }
